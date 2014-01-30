21bc5079dd764ef39ae157a02a1c1772cbb88a39
Joao Stein <joaosteing@gmail.com>
2014-01-30 14:37:20 -0800

    refactor: remove js.js file and add its content to application.js

--- js/application.js
+++ js/application.js
@@ -1,3 +1,48 @@
+jQuery(document).ready(function ($) {
+  $(window).stellar();
+  var links = $('.navigation').find('li');
+  slide = $('.slide');
+  button = $('.button');
+  mywindow = $(window);
+  htmlbody = $('html,body');
+
+  slide.waypoint(function (event, direction) {
+    dataslide = $(this).attr('data-section');
+
+    if (direction === 'down') {
+      $('.navigation li[data-section="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+    }
+    else {
+      $('.navigation li[data-section="' + dataslide + '"]').addClass('active').next().removeClass('active');
+    }
+  });
+
+  mywindow.scroll(function () {
+    if (mywindow.scrollTop() == 0) {
+      $('.navigation li[data-section="1"]').addClass('active');
+      $('.navigation li[data-section="2"]').removeClass('active');
+    }
+  });
+
+  function goToByScroll(dataslide) {
+    htmlbody.animate({
+      scrollTop: $('.slide[data-section="' + dataslide + '"]').offset().top
+    }, 1200, 'easeInOutQuint');
+  }
+
+  links.click(function (e) {
+    e.preventDefault();
+    dataslide = $(this).attr('data-section');
+    goToByScroll(dataslide);
+  });
+
+  button.click(function (e) {
+    e.preventDefault();
+    dataslide = $(this).attr('data-section');
+    goToByScroll(dataslide);
+  });
+});
+
 $(window).load(function () { // makes sure the whole site is loaded
   $("#status").fadeOut(); // will first fade out the loading animation
   $("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.

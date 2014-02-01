c2b50ec861452815d47328b04f6428c4c705954b
Joao Stein <joaosteing@gmail.com>
2014-02-01 09:37:10 -0800

    fix(general): remove console.log

--- js/application.js
+++ js/application.js
@@ -98,9 +98,7 @@ $(function () {
 });
 
 $(function() {
-  console.log('aeww');
   $('.nav-condensed-menu').on('click', function (e) {
-    console.log('ae');
     $('.navigation').toggleClass('notVisible');
     $('.navigation').toggleClass('visible');
     e.preventDefault();
